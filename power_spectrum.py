e6d890e415b2cd354b47f0473d1eb8b6f47b0172
avi <avirukt@gmail.com>
2019-04-11 21:09:41 -0700

    Streamline things

--- power_spectrum.py
+++ power_spectrum.py
@@ -1,9 +1,12 @@
 from numpy import *
 from numpy.random import rand,randn,randint
 from itertools import permutations
-from scipy.stats import binned_statistic
+from scipy.stats import binned_statistic,binom
+from matplotlib.pyplot import *
+import tensorflow as tf
+import tensorflow_probability as tfp
 
-colon=slice(None,None,None)
+colon=slice(None)
 
 def field_from_spectrum(ps,boxsize=128,n=1):
     f=randn(n,boxsize,boxsize)*exp(2j*pi*rand(n,boxsize,boxsize))
@@ -198,7 +201,7 @@ def norm_func(x,fn):
 
 def bin_by_count(k,n):
     ss={}
-    for i in range(len(k)):
+    for i in range(1,len(k)):
         if k[i] in ss.keys():
             ss[k[i]].append(i)
         else:
@@ -277,7 +280,249 @@ def renorm_count(x,fn,count=20):
     fk[0]=0
     return real(fft.fft2(fk.reshape((size,size))))
 
+def standardise_ps(x, standard, original=None, count=20):
+    n = x.shape[0]
+    d = x.ndim-1
+    size = x.shape[-1]
+    shape = [n]+[size]*(d-1)+[size//2+1]
+    factor = size**(d/2)
+    axes = tuple(range(1,d+1))
+    x = (fft.rfftn(x, axes=axes)/factor).reshape((n,-1))
+    f = abs(x)**2
+    new = zeros([1]+shape[1:])
+    build_ps(new,lambda x: array([standard(x)]))
+    new = new[0].flatten()
+    if originals is not None:
+        current = zeros(shape)
+        build_ps(current, originals, size, d)
+        current = current.reshape((n,-1))
+    else:
+        indices,edges = bin_by_count(k, count)
+        current = zeros(shape).reshape((n,-1))
+        for ind in indices:
+            current[:,ind] = mean(f[:,ind],axis=1).reshape((-1,1))
+    x *= sqrt(new/current)
+    x = x.reshape(shape)
+    return fft.irfftn(x,axes=axes)*factor                                                    
 
+def triangle_plot(samples, labels=None, title=None, nbins=100, bounds=None, expected=None, truth=None, figsize=4):
+    n = samples.shape[-1]
+    if bounds is None:
+        bounds = [(amin(s), amax(s)) for s in samples.T]
+    f,ax = subplots(n,n,gridspec_kw={"hspace": 0.05, "wspace": 0.05},figsize=(figsize*n,figsize*n))
+    if n==1:
+        ax = np.array([[ax]])
+    if title is not None:
+        suptitle(title)
+    bins = [linspace(*bounds[i],nbins+1) for i in range(n)]
+    for i in range(n):
+        counts = ax[i,i].hist(samples[:,i],bins=bins[i])[0]
+        if i<n-1:
+            ax[i,i].set_xticks([])
+        ax[i,i].set_yticks([])
+        ax[i,i].set_xlim(*bounds[i])
+        if expected is not None:
+            mesh = linspace(*bounds[i], 500)
+            expmesh = expected[i](mesh)
+            expmesh *= amax(counts)
+            ax[i,i].plot(mesh,expmesh,c="k")
+        if truth is not None:
+            ax[i,i].axvline(truth[i], c="r")
+        for j in range(i):
+            if i<n-1:
+                ax[i,j].set_xticks([])
+            if j>0:
+                ax[i,j].set_yticks([])
+            ax[i,j].hist2d(samples[:,j],samples[:,i], cmap="Blues", bins=[bins[j],bins[i]])
+            ax[-(i+1),-(j+1)].axis("off")
+            ax[i,j].set_ylim(*bounds[i])
+            ax[i,j].set_xlim(*bounds[j])
+            if truth is not None:
+                ax[i,j].scatter(truth[j],truth[i],c="r",marker="x")
+        ax[-1,i].set_xlabel(labels[i])
+        if i>0:
+            ax[i,0].set_ylabel(labels[i])
+        #return f
 
-def remove_linear():
-    pass
+def corners(samples,bins=30,prenorm=False,**kwargs):
+    if not iterable(bins):
+        bins = linspace(amin(samples),amax(samples),bins+1)
+    counts = histogram(samples,bins,**kwargs)[0]
+    if prenorm:
+        counts = counts/amax(counts)
+    x,y = [bins[0]], [0]
+    for i in range(len(counts)):
+        y += [counts[i], counts[i]]
+        x += [bins[i], bins[i+1]]
+    x += [bins[-1]]
+    y += [0]
+    return x,y
+
+def training_fn_generator(x,p,batch_size=32,buffer=1000):
+    def f():
+        dataset = tf.data.Dataset.from_tensor_slices((x.astype('float32'), p.astype('float32')))
+        dataset = dataset.repeat().shuffle(buffer).batch(batch_size)
+        return dataset
+    return f
+
+def testing_fn_generator(x,p,batch_size=32):
+    def f():
+        dataset = tf.data.Dataset.from_tensor_slices((x.astype('float32'), p.astype('float32')))
+        dataset = dataset.batch(batch_size)
+        return dataset
+    return f
+
+class LFI(tf.estimator.Estimator):
+    def plot_summaries(self, x, p, nbins=100, figsize=4):
+        testing_fn = testing_fn_generator(x,p)
+        stats = array([p["stat"] for p in self.predict(testing_fn)])
+        nbands = stats.shape[-1]
+        f,ax = subplots(nbands,nbands,sharex=True,sharey=True,gridspec_kw={"hspace": 0.05, "wspace": 0.05},figsize=(figsize*nbands,figsize*nbands))
+        if nbands==1:
+            ax = array([[ax]])
+        suptitle("Summary statistics")
+        for i in range(nbands):
+            for j in range(nbands):
+                ax[i,j].hist2d(p[:,j],stats[:,i],nbins,cmap="Blues")
+        for i in range(nbands):
+            ax[i,0].set_ylabel(f"statistic {i+1}")
+            ax[-1,i].set_xlabel(self.labels[i])
+        #return f
+    
+    def plot_posteriors(self, x, p, compare=None, **kwargs):
+        testing_fn = testing_fn_generator(x,p)
+        for i,m in enumerate(self.predict(testing_fn)):
+            if compare is not None:
+                kwargs["expected"] = compare(x[i])
+            yield triangle_plot(m["samples"], labels=self.labels, title=f"sample {i+1}", truth=p[i], **kwargs)
+    
+    def plot_ranks(self, x, p, nbins=None, figsize=6):
+        testing_fn = testing_fn_generator(x,p)
+        ranks = zeros(p.shape)
+        N,n = p.shape
+        for i,m in enumerate(self.predict(testing_fn)):
+            ranks[i] = sum(m["samples"]<p[i].reshape((1,-1)),axis=0)
+        if nbins is None:
+            nbins = int(N**.5)
+        f,ax = subplots(n,1,figsize=(figsize,2/3*figsize*n))
+        if n==1:
+            ax=[ax]
+        interval = binom.interval(0.9, N, 1/nbins)
+        edges = linspace(0,self.n_samples,nbins+1)
+        for i in range(n):
+            ax[i].hist(ranks[:,i],edges)
+            ax[i].axhspan(*interval, color="r", alpha=0.3)
+            ax[i].set_title(self.labels[i])
+            ax[i].set_xlabel("rank of truth among posterior samples")
+        #return f
+    
+    def __init__(self,
+                feature_columns,
+                label_columns,
+                n_mixture=10,
+                diag=False,
+                optimizer=tf.train.AdamOptimizer,
+                activation_fn=tf.nn.relu,
+                normalizer_fn=tf.contrib.layers.batch_norm,
+                dropout=0.2,
+                model_dir=None,
+                config=None,
+                n_samples = 2000,
+                model_fn=None):
+        
+        if model_fn is not None:
+            return tf.estimator.Estimator.__init__(self,model_fn=model_fn,
+                                             model_dir=model_dir,
+                                             config=config)
+
+        def _model_fn(features, labels, mode):
+            label_dimension = len(label_columns)
+            training=mode == tf.estimator.ModeKeys.TRAIN
+            # Builds the neural network
+            size = features.shape[-1]
+            d = len(features.shape)-1
+            conv = tf.reshape(features,tuple([-1]+[size]*d+[1]))
+            channels = 1
+            width = size
+            while width > 1:
+                width //= 2
+                channels *= 2**d
+                conv = tf.layers.conv2d(conv, channels, 2, strides=2, activation=tf.nn.leaky_relu)
+            dense=tf.reshape(conv,(-1,channels))
+            f = -int(-(channels/label_dimension)**.2)
+            for i in range(3):
+                channels //= f
+                dense = tf.contrib.layers.fully_connected(tf.layers.dropout(dense,rate=dropout,training=training),channels,activation_fn=tf.nn.leaky_relu)
+            channels //= f
+            dense = tf.contrib.layers.fully_connected(tf.layers.dropout(dense,rate=dropout,training=training),channels,activation_fn=tf.nn.tanh)
+            stat = tf.contrib.layers.fully_connected(tf.layers.dropout(dense,rate=dropout,training=training),label_dimension)
+            
+            net = tf.contrib.layers.fully_connected(stat, 128, activation_fn=tf.nn.tanh)
+            
+                # Size of the covariance matrix
+            if diag ==True:
+                size_sigma = label_dimension
+            else:
+                size_sigma = (label_dimension *(label_dimension +1) // 2)
+
+            # Create mixture components from network output
+            out_mu = tf.contrib.layers.fully_connected(net, label_dimension*n_mixture , activation_fn=None)
+            out_mu = tf.reshape(out_mu, (-1, n_mixture, label_dimension))
+
+            out_sigma = tf.contrib.layers.fully_connected(net, size_sigma * n_mixture, activation_fn=None)
+            out_sigma = tf.reshape(out_sigma, (-1, n_mixture, size_sigma))
+
+            out_p = tf.contrib.layers.fully_connected(net, n_mixture, activation_fn=None)
+
+            if diag == True:
+                sigma_mat = tf.nn.softplus(out_sigma)+1e-4
+                gmm = tfp.distributions.MixtureSameFamily(mixture_distribution=tfp.distributions.Categorical(logits=out_p),
+                              components_distribution=tfp.distributions.MultivariateNormalDiag(loc=out_mu,
+                                                                                scale_diag=sigma_mat))
+            else:
+                sigma_mat = tfp.distributions.matrix_diag_transform(tfp.distributions.fill_triangular(out_sigma), transform=tf.nn.softplus)
+                gmm = tfp.distributions.MixtureSameFamily(mixture_distribution=tfp.distributions.Categorical(logits=out_p),
+                             components_distribution=tfp.distributions.MultivariateNormalTriL(loc=out_mu,
+                                                                                scale_tril=sigma_mat))
+
+            predictions = {'mu': out_mu, 'sigma': sigma_mat, 'p':out_p, 'stat':stat}
+
+            if mode == tf.estimator.ModeKeys.PREDICT:
+                y = gmm.sample(n_samples)
+                predictions['samples'] = tf.transpose(y,[1,0,2])
+
+                return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions,
+                                                  export_outputs={'pdf': tf.estimator.export.PredictOutput(predictions),
+                                                                  'samples': tf.estimator.export.PredictOutput(y),
+                                                                  tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: tf.estimator.export.PredictOutput(y)})
+            label_layer = labels
+
+            # Compute and register loss function
+            loss = - tf.reduce_mean(gmm.log_prob(label_layer),axis=0)
+            tf.losses.add_loss(loss)
+            total_loss = tf.losses.get_total_loss(add_regularization_losses=True)
+
+            train_op = None
+            eval_metric_ops = None
+
+            # Define optimizer
+            if mode == tf.estimator.ModeKeys.TRAIN:
+                update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+                with tf.control_dependencies(update_ops):
+                    train_op = optimizer(learning_rate=0.0002).minimize(loss=total_loss,
+                                                global_step=tf.train.get_global_step())
+                tf.summary.scalar('loss', loss)
+            elif mode == tf.estimator.ModeKeys.EVAL:
+                eval_metric_ops = { "log_p": loss}
+
+            return tf.estimator.EstimatorSpec(mode=mode,
+                                              predictions=predictions,
+                                              loss=total_loss,
+                                              train_op=train_op,
+                                              eval_metric_ops=eval_metric_ops)
+
+        tf.estimator.Estimator.__init__(self,model_fn=_model_fn,
+                                             model_dir=model_dir,
+                                             config=config)
+        self.labels = label_columns
+        self.n_samples = n_samples
